eb221d1e5b8984e0fc464d40701e6d253352d0f6
kiling <kiling@bk.ru>
2025-08-17 13:30:31 +0300

    init webapp template

---
+++ webapp/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <button className="btn btn-primary">Кнопка Bootstrap1</button>
+    </div>
+  );
+}
+
+export default App;
